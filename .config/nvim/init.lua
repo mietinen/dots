6f90a0d280b644ae36b42e56214d915c1e0cb496
Aleksander Mietinen <aleksander@mietinen.net>
2025-10-03 07:47:04 +0200

    lspconfig

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -95,7 +95,7 @@ vim.cmd.highlight('Normal guibg=NONE ctermbg=NONE')
 -- lazy.nvim (lua/plugins)
 -- -----------------------------------------------------------------------------
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system { 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath, }
 end
 vim.opt.rtp:prepend(lazypath)
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,24 +1,35 @@
-local custom_attach = function(client, bufnr)
-    local opts = {buffer = bufnr, silent = true}
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-    client.server_capabilities.semanticTokensProvider = nil
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('LspStuff', { clear = true }),
+    callback = function(args)
+        local opts = { buffer = args.buf, silent = true }
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '[d', function() vim.diagnostic.jump({count= -1,float = true}) end, opts)
+        vim.keymap.set('n', ']d', function() vim.diagnostic.jump({count= 1,float = true}) end, opts)
+        client.server_capabilities.semanticTokensProvider = nil
+    end
+})
+
+vim.diagnostic.config({
+    virtual_text = true,
+    float = {
+        border = "rounded",
+        source = true
+    },
+})
+
+local lsp_setup = function(server, config)
+    vim.lsp.config(server, config)
+    if vim.fn.executable(vim.lsp.config[server].cmd[1]) ~= 0 then
+        vim.lsp.enable(server)
+    end
 end
 
 return {
     'neovim/nvim-lspconfig',
     config = function()
-        local lsp = require('lspconfig')
-        local lsp_setup = function(server, config)
-            local cmd = config.cmd or lsp[server].document_config.default_config.cmd or {''}
-            config.on_attach = config.on_attach or custom_attach
-            if vim.fn.executable(cmd[1]) ~= 0 then
-                lsp[server].setup(config)
-            end
-        end
         lsp_setup('gopls', {})
         lsp_setup('rust_analyzer', {})
         lsp_setup('bashls', {})
@@ -28,10 +39,13 @@ return {
                 Lua = {
                     runtime = { version = 'LuaJIT' },
                     diagnostics = {
-                        globals = { 'vim', 'awesome', 'client', 'screen', 'root' },
                         disable = { 'lowercase-global' },
                     },
-                    workspace = { library = vim.api.nvim_get_runtime_file('', true), checkThirdParty = false },
+                    workspace = {
+                        -- library = vim.api.nvim_get_runtime_file('', true),
+                        library = { vim.env.VIMRUNTIME },
+                        checkThirdParty = false
+                    },
                 },
             },
         })
